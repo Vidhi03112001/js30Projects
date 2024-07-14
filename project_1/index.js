43fccc215c99f89a14a1d87f1e8feb80b47ff02e
Vidhi Tiwari <tiwarividhi1947@gmail.com>
2024-07-15 00:17:14 +0530

    Adding JS file of project 1
    Signed-off-by: Vidhi Tiwari <tiwarividhi1947@gmail.com>

--- project_1/index.js
+++ project_1/index.js
@@ -23,9 +23,9 @@ function setDate() {
 
 function updateClock() {
   const now = new Date();
-  const hours = String(now.getHours());
-  const minutes = String(now.getMinutes());
-  const seconds = String(now.getSeconds());
+  const hours = String(now.getHours()).padStart(2,'0');
+  const minutes = String(now.getMinutes()).padStart(2,'0');
+  const seconds = String(now.getSeconds()).padStart(2,'0');
   
   const formattedTime = `${hours}:${minutes}:${seconds}`;
 
